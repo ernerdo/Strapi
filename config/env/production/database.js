66884e226e6c40514bd15300383ca7357dfe2f88
Ernesto Herrera <jesh40@hotmail.es>
2022-11-05 21:33:53 -0600

    add ssl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,6 +7,9 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
     debug: false,
   },
