e3087cb4a9b76b7455a5eb8453a06430abd21883
Ernesto Herrera <jesh40@hotmail.es>
2022-11-06 09:39:23 -0600

    add sqlite and config database local

--- config/database.js
+++ config/database.js
@@ -1,13 +1,11 @@
-module.exports = ({ env }) => ({
+const path = require("path");
+
+module.exports = () => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
