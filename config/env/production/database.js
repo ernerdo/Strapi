5c3ea13400c99228cb515324a11d6792b1325219
Ernesto Herrera <jesh40@hotmail.es>
2022-11-06 08:39:23 -0600

    change database sqlite for pg

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        ca: env("DATABASE_CA"),
+      },
     },
+    debug: false,
   },
 });
