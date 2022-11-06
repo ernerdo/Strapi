5d2edd3cb7006d74a40f0d83182d0151620dfea1
Ernesto Herrera <jesh40@hotmail.es>
2022-11-06 09:21:17 -0600

    ignore data docker

--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {},
+    watchIgnoreFiles: ["**/dump/**", "**/ignoredfolder/**"],
   },
 });
